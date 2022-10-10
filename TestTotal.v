62ccab57b274793917f7f0331017329a0bf57390
sebashm14 <111079213+sebashm14@users.noreply.github.com>
2022-10-10 10:48:46 -0600

    Update and rename Modulo de Test to TestTotal.v
    Modulo para simular el proyecto y verificar funcionamiento completo.

--- TestTotal.v
+++ TestTotal.v
@@ -11,31 +11,31 @@ module TestTotal;
 	wire [3:0] OUTbinario;
 	wire Q1;
 	wire Q2;
+	wire an3;
+	wire an4;
 
 	// Instantiate the Unit Under Test (UUT)
 	ProyectoCOMPL uut (
 		.clk(clk), 
-		.rst(rst), 
-		.load(load), 
+		.rst(rst),  
 		.Data_in(Data_in), 
 		.outDisplay(outDisplay), 
 		.OUTbinario(OUTbinario), 
 		.Q1(Q1), 
-		.Q2(Q2)
+		.Q2(Q2),
+		.an3(an3), 
+		.an4(an4)
 	);
 
 	initial begin
 		// Initialize Inputs
 		clk = 0;
 		rst = 0;
-		load = 0;
 		Data_in = 0;
 
 		#20 rst=1;
 		#20 rst=0;
-		#20 Data_in = 4'b1010;
-		#20 load = 1;
-		#20 load = 0;
+		#20 Data_in = 4'b1011;
 		
 	end	
 	initial
