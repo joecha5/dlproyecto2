871a3d2a037614794c4f34bff4999d16181d3192
sebashm14 <111079213+sebashm14@users.noreply.github.com>
2022-10-10 10:27:20 -0600

    Update and rename Contador para tasa de refresco to RefreshCounter.v

---
+++ RefreshCounter.v
@@ -0,0 +1,16 @@
+module Cont(clk, rst, Cout);
+	input wire clk, rst;
+	output reg[18:0] Cout;
+
+always@(posedge clk or posedge rst)
+		if(rst)
+			Cout = 19'd0;
+			
+		else if(Cout == 19'd480000)
+			Cout = 19'd0;
+			
+		else 
+			Cout = Cout + 19'd1;
+ 
+endmodule
+	   
